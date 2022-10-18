44996f1f238b6176ffc9ae433a5aefb9d32a514d
haider <rabeeahhaydeer@gmail.com>
2022-10-18 12:28:05 +0300

    create the nav__tabs-primary.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+//?nav-tabs
+
+let tab_header = document.querySelector(".tab-header");
+
+let tabli = document.querySelectorAll(".tab-header li");
+let tab_inner = document.querySelectorAll(".tab-inner");
+console.log(tabli);
+
+tab_header.addEventListener("click", function (e) {
+  let id = e.target.dataset.id;
+  if (id) {
+    tabli.forEach(function (li_elem) {
+      li_elem.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    tab_inner.forEach(function (tabs) {
+      tabs.classList.remove("active");
+    });
+    activetab = document.getElementById(id);
+    activetab.classList.add("active");
+  }
+});
