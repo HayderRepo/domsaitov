1462399c47f0893c89737f14a3958b02e10366c5
haider <rabeeahhaydeer@gmail.com>
2022-10-20 04:14:13 +0300

    finsh  first half of website layout and make responsiv.

--- js/main.js
+++ js/main.js
@@ -7,24 +7,55 @@ collapsibles.forEach((item) =>
 
 //?nav-tabs
 
-let tab_header = document.querySelector(".tab-header");
 
-let tabli = document.querySelectorAll(".tab-header li");
-let tab_inner = document.querySelectorAll(".tab-inner");
-console.log(tabli);
+//!nav primary
 
-tab_header.addEventListener("click", function (e) {
+let tab_header_p = document.querySelector(".tab-header__primary");
+
+let tabli_p = document.querySelectorAll(".tab-header__primary li");
+let tab_inner_p = document.querySelectorAll(".tab-inner_p");
+console.log(tabli_p);
+
+tab_header_p.addEventListener("click", function (e) {
   let id = e.target.dataset.id;
   if (id) {
-    tabli.forEach(function (li_elem) {
+    tabli_p.forEach(function (li_elem) {
       li_elem.classList.remove("active");
     });
     e.target.classList.add("active");
 
-    tab_inner.forEach(function (tabs) {
+    tab_inner_p.forEach(function (tabs) {
       tabs.classList.remove("active");
     });
     activetab = document.getElementById(id);
     activetab.classList.add("active");
   }
 });
+
+
+//!secondary nav-tabs
+
+
+let tab_header_s = document.querySelector(".tab-header__secondary");
+
+let tabli_s = document.querySelectorAll(".tab-header__secondary li");
+let tab_inner_s = document.querySelectorAll(".tab-inner_s");
+console.log(tabli_s);
+
+tab_header_s.addEventListener("click", function (e) {
+  let id = e.target.dataset.id;
+  if (id) {
+    tabli_s.forEach(function (li_elem) {
+      li_elem.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    tab_inner_s.forEach(function (tabs) {
+      tabs.classList.remove("active");
+    });
+    activetab = document.getElementById(id);
+    activetab.classList.add("active");
+  }
+});
+
+
